62c65b958d495cccf109b0224fd65b10562a738c
fsRoger <fsroger@outlook.com>
2024-04-01 10:48:09 -0300

    final

--- src/services/productServices.js
+++ src/services/productServices.js
@@ -1,19 +1,25 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',  // ou 'http://localhost:3000' para iOS
+  baseURL: 'http://192.168.0.111:3000/',
 });
 
 export function getAllGames() {
-  return api.get('/product');
-  console.log("prodihg", product)
+  return api.get('/product')
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.error('Erro ao obter os dados:', error);
+      throw error;
+    });
 }
 
-export function searchProduct(title) {
-  return api.get(`/product/search?title=${title}`);
-}
+//export function searchProduct(title) {
+//  return api.get(`/product/search?title=${title}`);
+//}
 
-export async function getProductById(id) {
-  const response = await api.get(`/product/${id}`);
-  return response;
-}
+//export async function getProductById(id) {
+//  const response = await api.get(`/product/${id}`);
+//  return response;
+//}
